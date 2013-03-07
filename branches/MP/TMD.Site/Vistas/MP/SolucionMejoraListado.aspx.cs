e0877160d062da3976ff045f88bebfdacf5b6c37
alyssa.quinto@gmail.com <alyssa.quinto@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-07 11:45:14 +0000

    Solucion

---
+++ branches/MP/TMD.Site/Vistas/MP/SolucionMejoraListado.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TMD.MP.Controlador;
+using TMD.Entidades;
+using TMD.MP.Comun;
+
+namespace TMD.MP.Site.Privado
+{
+    public partial class SolucionMejoraLista : System.Web.UI.Page
+    {
+        public SolucionMejoraControlador solucionMejoraControlador = new SolucionMejoraControlador();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack) {
+                CargarPropuesta();
+                CargarSolucionMejoraListado();
+            }
+        }
+
+        protected void CargarPropuesta() {
+            
+        }
+
+        protected void CargarSolucionMejoraListado() {
+            SolucionMejoraEntidad oSolucionMejoraFiltro = new SolucionMejoraEntidad();
+
+        }
+
+        protected List<SolucionMejoraEntidad> ObtenerSolucionMejoraListado()
+        {
+            
+                return null;
+        }
+
+        protected void lbtnBuscar_Click(object sender, EventArgs e)
+        {
+            CargarSolucionMejoraListado();
+        }
+
+        protected void gvwSolucionMejoraListado_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "EditarSolucion") {
+               
+            }else if(e.CommandName == "EliminarPropuesta"){
+               
+            }
+        }
+
+        protected void BorrarSolucionMejora(SolucionMejoraEntidad oSolucionMejora)
+        {
+            
+        }
+
+        protected void ibtnAgregarSolucion_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void ibtnSalir_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(Paginas.TMD_MP_Inicio, true);
+        }
+    }
+}
