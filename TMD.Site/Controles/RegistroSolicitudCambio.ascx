5007ecaafad878a98146d1d94865ec5e85e90efe
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-09 19:49:05 +0000

    CF : + Se agrego el control de registo de solicitud de cambio

---
+++ TMD.Site/Controles/RegistroSolicitudCambio.ascx
@@ -0,0 +1,49 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="RegistroSolicitudCambio.ascx.cs" Inherits="TMD.CF.Site.Controles.RegistroSolicitudCambio" %>
+<asp:Panel runat="server">
+<p>
+    Solicitud de Cambio</p>
+<p>
+    Codigo:
+    <asp:Label ID="lblCodigo" runat="server"></asp:Label>
+</p>
+<p>
+    Nombre:
+    <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+</p>
+<p>
+    Proyecto:
+    <asp:DropDownList ID="ddlProyecto" runat="server" AutoPostBack="True" 
+        onselectedindexchanged="ddlProyecto_SelectedIndexChanged">
+    </asp:DropDownList>
+</p>
+<p>
+    Linea Base:
+    <asp:DropDownList ID="ddlLineaBase" runat="server" AutoPostBack="True" 
+        onselectedindexchanged="ddlLineaBase_SelectedIndexChanged">
+    </asp:DropDownList>
+</p>
+<p>
+    Elemento Configuracion:  
+    <asp:DropDownList ID="ddlElementoConfiguracion" runat="server">
+    </asp:DropDownList>
+</p>
+<p>
+    Prioridad:
+    <asp:DropDownList ID="ddlPrioridad" runat="server">
+    </asp:DropDownList>
+</p>
+<p>
+  Estado:
+    <asp:DropDownList ID="ddlEstado" runat="server">
+    </asp:DropDownList>
+</p>
+<p>
+    <asp:Button ID="btnGrabar" runat="server" onclick="btnGrabar_Click" 
+        Text="Grabar" />
+    <asp:Button ID="btnSalir" runat="server" Text="Salir" />
+</p>
+<p>
+    &nbsp;</p>
+<p>
+    &nbsp;</p>
+</asp:Panel>
---
+++ TMD.Site/Controles/RegistroSolicitudCambio.ascx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TMD.CF.Site.Controladora;
+using TMD.Entidades;
+using TMD.Core.Extension;
+using TMD.CF.Site.Util;
+
+namespace TMD.CF.Site.Controles
+{
+    public partial class RegistroSolicitudCambio : System.Web.UI.UserControl
+    {
+        LineaBaseControladora _lineaBaseControladora = new LineaBaseControladora();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CargarControles()
+        {
+            ddlProyecto.DataSource = null;
+            ddlLineaBase.DataSource = null;
+            ddlElementoConfiguracion.DataSource = null;
+            ddlEstado.DataSource = null;
+            ddlPrioridad.DataSource = null;
+        }
+
+        protected void ddlProyecto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ddlLineaBase_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGrabar_Click(object sender, EventArgs e)
+        {
+            SolicitudCambio solicitudCambio = CrearSolicitud();
+        }
+
+        private SolicitudCambio CrearSolicitud()
+        {
+            return new SolicitudCambio
+                {
+                    Nombre = txtNombre.Text,
+                    ProyectoFase = new ProyectoFase { Proyecto = new Proyecto { Id = ddlProyecto.SelectedValue.ToInt() } },
+                    LineaBase = new LineaBase{ Id = ddlLineaBase.SelectedValue.ToInt()},
+                    ElementoConfiguracion = new LineaBaseElementoConfiguracion{ Id = ddlElementoConfiguracion.SelectedValue.ToInt()},
+                    Estado = ddlEstado.SelectedValue.ToInt(),
+                    Prioridad = ddlPrioridad.SelectedValue.ToInt(),
+                    Usuario = SesionFachada.Usuario
+                };
+        }
+    }
+}
