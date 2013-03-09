37afe99a83ad27f9ed7c7196a95f81fc5f6b28d8
jacardenasm@gmail.com <jacardenasm@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-09 23:08:06 +0000

    CF : * Cambios masivos para corregir la compilación de proyecto

--- TMD.CF.AccesoDatos/Map/InformeCambioMap.cs
+++ TMD.CF.AccesoDatos/Map/InformeCambioMap.cs
@@ -8,18 +8,17 @@ using TMD.CF.AccesoDatos.Util;
 
 namespace TMD.CF.AccesoDatos.Map
 {
-    static class SolicitudCambioMap
+    static class InformeCambioMap
     {
-        public static SolicitudCambio Select(IDataReader reader)
+        public static InformeCambio Select(IDataReader reader)
         {
-            return new SolicitudCambio
+            return new InformeCambio
             {
                 Id = reader.GetInt("CODIGO"),
                 Nombre = reader.GetString("NOMBRE"),
                 FechaAprobacion = reader.GetDateTime("FECHA_APROBACION"),
                 FechaRegistro = reader.GetDateTime("FECHA_REGISTRO"),
                 Estado = reader.GetInt("ESTADO"),
-                Prioridad = reader.GetInt("PRIORIDAD"),
                 Motivo = reader.GetString("MOTIVO")
             };
         }
