f8e863d77ce3fb21e68aa9a8a9d7c3d4972f5964
caperaltasanchez@gmail.com <caperaltasanchez@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-09 17:07:59 +0000

    CF: + Se agregó la clase e interface SolicitudCambioData.

---
+++ TMD.CF.AccesoDatos/Implementacion/SolicitudCambioData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMD.CF.AccesoDatos.Contrato;
+using TMD.CF.AccesoDatos.Core;
+
+namespace TMD.CF.AccesoDatos.Implementacion
+{
+    public class SolicitudCambioData : DataBase, ISolicitudCambioData
+    {
+
+        public SolicitudCambioData(String connectionString)
+            : base(connectionString)
+        {
+        }
+
+        public void Agregar(Entidades.SolicitudCambio solicitudCambio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Aprobar(Entidades.SolicitudCambio solicitudCambio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entidades.SolicitudCambio> ListarPorProyectoLineaBase(Entidades.SolicitudCambio solicitudCambio)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
