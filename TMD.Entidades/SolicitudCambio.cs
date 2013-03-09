6553b77a8d19bc1deb79546f28f92f847ee24985
caperaltasanchez@gmail.com <caperaltasanchez@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-09 16:58:48 +0000

    CF: + Se agregó entidad Solicitud Cambio

---
+++ TMD.Entidades/SolicitudCambio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TMD.Entidades
+{
+    public class SolicitudCambio
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public ProyectoFase ProyectoFase { get; set; }
+        public LineaBase LineaBase { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public LineaBaseElementoConfiguracion ElementoConfiguracion { get; set; }
+        public int Estado { get; set; }
+        public int Prioridad { get; set; }
+        public string Motivo { get; set; }
+        public DateTime FechaAprobacion { get; set; }
+    }
+}
