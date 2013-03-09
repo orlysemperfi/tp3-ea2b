d0460b973ff4aed823ca477febce9e5c28f5be53
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-09 19:34:23 +0000

    CF : + Se agrego la clase Cadena

---
+++ TMD.Core/Extension/Cadena.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TMD.Core.Extension
+{
+    public static class Cadena
+    {
+        public static int ToInt(this string valor)
+        {
+            int numero = 0;
+            Int32.TryParse(valor, out numero);
+            return numero;
+        }
+    }
+}
