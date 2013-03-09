590847e55e6b4de47f1d8eb8dd05b0e3ee0e6b11
jacardenasm@gmail.com <jacardenasm@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-09 19:12:20 +0000

    CF : Agregada la clase SolicitudCambioMap

---
+++ TMD.CF.AccesoDatos/Map/SolicitudCambioMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMD.Entidades;
+using System.Data;
+using TMD.CF.AccesoDatos.Util;
+
+namespace TMD.CF.AccesoDatos.Map
+{
+    static class SolicitudCambioMap
+    {
+        public static SolicitudCambio Select(IDataReader reader)
+        {
+            return new SolicitudCambio
+            {
+                codigo = reader.GetInt("CODIGO"),
+                Nombre = reader.GetString("NOMBRE"),
+                FechaAprobacion = reader.GetDateTime("FECHA_APROBACION"),
+                FechaRegistro = reader.GetDateTime("FECHA_REGISTRO"),
+                Estado = reader.GetInt("ESTADO"),
+                Prioridad = reader.GetInt("PRIORIDAD"),
+                Motivo = reader.GetString("MOTIVO")
+            };
+        }
+    }
+}
