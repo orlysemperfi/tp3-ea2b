74ed4f1924e71bb6651561fccbb2f5288fae2136
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-09 20:19:44 +0000

    CF :  * Se modifico el registro de solicitud se agrego el mertodo listar proyetco

--- TMD.Site/Controles/RegistroSolicitudCambio.ascx.cs
+++ TMD.Site/Controles/RegistroSolicitudCambio.ascx.cs
@@ -8,40 +8,56 @@ using TMD.CF.Site.Controladora;
 using TMD.Entidades;
 using TMD.Core.Extension;
 using TMD.CF.Site.Util;
+using TMD.CF.Site.Controladora.CF;
 
 namespace TMD.CF.Site.Controles
 {
     public partial class RegistroSolicitudCambio : System.Web.UI.UserControl
     {
-        LineaBaseControladora _lineaBaseControladora = new LineaBaseControladora();
+        public delegate void GraboHandler();
+        public event GraboHandler EventoGrabo;
+
+        protected virtual void OnEventoGrabo()
+        {
+            GraboHandler handler = EventoGrabo;
+            if (handler != null) handler();
+        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            CargarControles();
         }
 
         private void CargarControles()
         {
-            ddlProyecto.DataSource = null;
-            ddlLineaBase.DataSource = null;
-            ddlElementoConfiguracion.DataSource = null;
-            ddlEstado.DataSource = null;
-            ddlPrioridad.DataSource = null;
+            ddlProyecto.DataSource = LineaBaseControladora.ListarProyectoPorUsuario(1);
+            ddlProyecto.DataTextField = "Nombre";
+            ddlProyecto.DataValueField = "Id";
+            ddlProyecto.DataBind();
         }
 
         protected void ddlProyecto_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            ddlLineaBase.DataSource = LineaBaseControladora.LineaBaseListarPorProyecto(ddlProyecto.SelectedValue.ToInt());
+            ddlLineaBase.DataTextField = "Nombre";
+            ddlLineaBase.DataValueField = "Id";
+            ddlLineaBase.DataBind();
         }
 
         protected void ddlLineaBase_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            //ddlElementoConfiguracion.DataSource = LineaBaseControladora.ElementoConfiguracionListarPorFase()
+            //ddlEstado.DataSource = null;
+            //ddlPrioridad.DataSource = null;
         }
 
         protected void btnGrabar_Click(object sender, EventArgs e)
         {
             SolicitudCambio solicitudCambio = CrearSolicitud();
+
+            SolicitudCambioControladora.Agregar(solicitudCambio);
+
+            OnEventoGrabo();
         }
 
         private SolicitudCambio CrearSolicitud()
