beb6098e5f08b6b2a16bb7939faefd70b8993eeb
caperaltasanchez@gmail.com <caperaltasanchez@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-09 17:23:44 +0000

    CF: + Se agregó la clase e interface de Solicitud Cambio Lógica.

---
+++ TMD.CF.LogicaNegocios/Contrato/ISolicitudCambioLogica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMD.Entidades;
+
+namespace TMD.CF.LogicaNegocios.Contrato
+{
+    public interface ISolicitudCambioLogica
+    {
+        void Agregar(SolicitudCambio solicitudCambio);
+        void Aprobar(SolicitudCambio solicitudCambio);
+        List<SolicitudCambio> ListarPorProyectoLineaBase(SolicitudCambio solicitudCambio);
+        SolicitudCambio ObtenerPorId(int id);
+    }
+}
---
+++ TMD.CF.LogicaNegocios/Implementacion/SolicitudCambioLogica.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMD.Entidades;
+using TMD.CF.LogicaNegocios.Contrato;
+
+namespace TMD.CF.LogicaNegocios.Implementacion
+{
+    public class SolicitudCambioLogica: ISolicitudCambioLogica
+    {
+
+        private readonly ISolicitudCambioLogica _solicitudCambioData;
+        
+        public SolicitudCambioLogica(ISolicitudCambioLogica solicitudCambioData)
+        {
+            _solicitudCambioData = solicitudCambioData;
+        }
+
+        public void Agregar(SolicitudCambio solicitudCambio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Aprobar(SolicitudCambio solicitudCambio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SolicitudCambio> ListarPorProyectoLineaBase(SolicitudCambio solicitudCambio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SolicitudCambio ObtenerPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
