2daa4cf2840e736837d76e99024606dc6e49e218
jacardenasm@gmail.com <jacardenasm@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-09 19:01:31 +0000

    CF : * Agregado el metodo Aprobar para la solicitud de cambio

--- branches/CF/TMD.CF.AccesoDatos/Implementacion/SolicitudCambioData.cs
+++ branches/CF/TMD.CF.AccesoDatos/Implementacion/SolicitudCambioData.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using TMD.CF.AccesoDatos.Contrato;
 using TMD.CF.AccesoDatos.Core;
+using TMD.CF.AccesoDatos.Contrato;
+using TMD.Entidades;
+using System.Data.Common;
+using System.Data;
+using TMD.CF.AccesoDatos.Map;
 
 namespace TMD.CF.AccesoDatos.Implementacion
 {
@@ -22,8 +26,26 @@ namespace TMD.CF.AccesoDatos.Implementacion
 
         public void Aprobar(Entidades.SolicitudCambio solicitudCambio)
         {
+            using (DbCommand command = DB.GetStoredProcCommand("USP_SOLICITUD_CAMBIO_ESTADO_UPD"))
+            {
+                DB.AddInParameter(command, "@CODIGO", DbType.Int32, solicitudCambio.codigo);
+
+                if (solicitudCambio.Estado == 3)
+                {
+                    using (DbCommand command1 = DB.GetStoredProcCommand("USP_SOLICITUD_CAMBIO_MOTIVO_UPD"))
+                    {
+                        DB.AddInParameter(command1, "@CODIGO", DbType.Int32, solicitudCambio.codigo);
+                        DB.AddInParameter(command1, "@MOTIVO", DbType.String, solicitudCambio.Motivo);
+                        DB.ExecuteNonQuery(command1);
+                    }
+                }
+
+                DB.AddInParameter(command, "@ESTADO", DbType.Int32, solicitudCambio.Estado);
+                DB.ExecuteNonQuery(command);
+            }
             throw new NotImplementedException();
         }
+
 
         public List<Entidades.SolicitudCambio> ListarPorProyectoLineaBase(Entidades.SolicitudCambio solicitudCambio)
         {
