0d23ce7fe1564f8d3ca3a5f27dcdf1092f61ce88
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-09 19:09:25 +0000

    CF : + Se agrego la funcionalidad de caching

---
+++ branches/CF/TMD.Core/Caching/ICacheAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TMD.Core.Caching
+{
+    public interface ICacheAdapter
+    {
+        void AddItem<T>(string name, T value, Double minutes = 1440);
+        T GetItem<T>(string name);
+        T Resolve<R, T>(string name, R param, Func<R, T> methodCall);
+        T Resolve<T>(string name, Func<T> methodCall);
+        void SetItem<T>(string name, T value, Double minutes = 1440);
+        T ResolveList<R, T>(string name, R param, Func<R, T> methodCall, Func<T, bool> validation);
+    }
+}
