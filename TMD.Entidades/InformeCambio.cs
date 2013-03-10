18cd442a037b4f025e3091a32a0d7448cbc7f267
jacardenasm@gmail.com <jacardenasm@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-10 00:45:42 +0000

    CF : * Limpieza de directorios

--- TMD.Entidades/InformeCambio.cs
+++ TMD.Entidades/InformeCambio.cs
@@ -7,6 +7,7 @@ namespace TMD.Entidades
 {
     public class InformeCambio
     {
+        // Prueba
         public int Id { get; set; }
         public string Nombre { get; set; }
         public SolicitudCambio Solicitud { get; set; }
