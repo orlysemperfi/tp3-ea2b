149568c42fc2449790a8763a8ecd6b0988de9927
jacardenasm@gmail.com <jacardenasm@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-10 00:16:25 +0000

    CF : +Agregada la entidad InformeCambio.cs

---
+++ branches/CF/TMD.Entidades/InformeCambio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TMD.Entidades
+{
+    public class InformeCambio
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public SolicitudCambio Solicitud { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DateTime FechaAprobacion { get; set; }
+        public string EstimacionCosto { get; set; }
+        public string EstimacionEsfuerzo { get; set; }
+        public string Recursos { get; set; }
+        public int Estado { get; set; }
+        public string Motivo { get; set; }
+    }
+}
