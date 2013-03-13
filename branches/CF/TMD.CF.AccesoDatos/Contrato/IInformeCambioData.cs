8f122572840271d92c1f73db95dacc527f8a1f36
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-13 04:17:34 +0000

    CF : * Se agrego comentarios a las clases de informe de cambios

--- branches/CF/TMD.CF.AccesoDatos/Contrato/IInformeCambioData.cs
+++ branches/CF/TMD.CF.AccesoDatos/Contrato/IInformeCambioData.cs
@@ -6,11 +6,35 @@ using TMD.Entidades;
 
 namespace TMD.CF.AccesoDatos.Contrato
 {
+    /// <summary>
+    /// Contrato  del Acceso a datos de la entidad informe de cambio.
+    /// </summary>
     public interface IInformeCambioData
     {
+        /// <summary>
+        /// Agrega un informe de cambio
+        /// </summary>
+        /// <param name="informeCambio">Objeto informe a agregar</param>
         void Agregar(InformeCambio informeCambio);
+
+        /// <summary>
+        /// Aprueba un informe de cambio
+        /// </summary>
+        /// <param name="informeCambio">Objeto informe a aprobar</param>
         void Aprobar(InformeCambio informeCambio);
+
+        /// <summary>
+        /// Lista los informes de una linea base
+        /// </summary>
+        /// <param name="informeCambio">Linea Base</param>
+        /// <returns>Lista informes de cambio</returns>
         List<InformeCambio> ListarPorLineaBase(InformeCambio informeCambio);
+
+        /// <summary>
+        /// Obtiene un informe de cambio por el id
+        /// </summary>
+        /// <param name="id">Id informe</param>
+        /// <returns>Informe de cambio</returns>
         InformeCambio ObtenerPorId(int id);
     }
 }
