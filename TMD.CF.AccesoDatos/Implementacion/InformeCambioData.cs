24e361a3b22b0b6d339eb63a419872a34c13de8d
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-13 04:17:34 +0000

    CF : * Se agrego comentarios a las clases de informe de cambios

--- TMD.CF.AccesoDatos/Implementacion/InformeCambioData.cs
+++ TMD.CF.AccesoDatos/Implementacion/InformeCambioData.cs
@@ -11,6 +11,9 @@ using TMD.CF.AccesoDatos.Map;
 
 namespace TMD.CF.AccesoDatos.Implementacion
 {
+    /// <summary>
+    /// Implmentacion  del Acceso a datos de la entidad informe de cambio.
+    /// </summary>
     public class InformeCambioData : DataBase, IInformeCambioData
     {
         public InformeCambioData(String connectionString)
@@ -18,21 +21,39 @@ namespace TMD.CF.AccesoDatos.Implementacion
         {
         }
 
+        /// <summary>
+        /// Agrega un informe de cambio
+        /// </summary>
+        /// <param name="informeCambio">Objeto informe a agregar</param>
         public void Agregar(InformeCambio informeCambio)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Aprueba un informe de cambio
+        /// </summary>
+        /// <param name="informeCambio">Objeto informe a aprobar</param>
         public void Aprobar(InformeCambio informeCambio)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Lista los informes de una linea base
+        /// </summary>
+        /// <param name="informeCambio">Linea Base</param>
+        /// <returns>Lista informes de cambio</returns>
         public List<InformeCambio> ListarPorLineaBase(InformeCambio informeCambio)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Obtiene un informe de cambio por el id
+        /// </summary>
+        /// <param name="id">Id informe</param>
+        /// <returns>Informe de cambio</returns>
         public InformeCambio ObtenerPorId(int id)
         {
             throw new NotImplementedException();
