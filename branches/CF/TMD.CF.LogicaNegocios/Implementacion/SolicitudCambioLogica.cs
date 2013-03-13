71533558f605063da94b6446b6f033b7e8411b07
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-13 04:34:13 +0000

    CF : * SE agrego comentarios a la clase SolicitudCambioLogica

--- branches/CF/TMD.CF.LogicaNegocios/Implementacion/SolicitudCambioLogica.cs
+++ branches/CF/TMD.CF.LogicaNegocios/Implementacion/SolicitudCambioLogica.cs
@@ -6,6 +6,9 @@ using TMD.Entidades;
 
 namespace TMD.CF.LogicaNegocios.Implementacion
 {
+    /// <summary>
+    /// Implementacion  de la logica de negocios de la entidad solicitud de cambio.
+    /// </summary>
     public class SolicitudCambioLogica: ISolicitudCambioLogica
     {
 
@@ -16,26 +19,49 @@ namespace TMD.CF.LogicaNegocios.Implementacion
             _solicitudCambioData = solicitudCambioData;
         }
 
+        /// <summary>
+        /// Agrega una solicitud de cambio
+        /// </summary>
+        /// <param name="informeCambio">Objeto Solicitud a agregar</param>
         public void Agregar(SolicitudCambio solicitudCambio)
         {
             _solicitudCambioData.Agregar(solicitudCambio);
         }
 
+        /// <summary>
+        /// Aprueba una solicitud de cambio
+        /// </summary>
+        /// <param name="informeCambio">Objeto Solicitud a aprobar</param>
         public void Aprobar(SolicitudCambio solicitudCambio)
         {
             _solicitudCambioData.Aprobar(solicitudCambio);
         }
 
+        /// <summary>
+        /// Lista las solicitudes por proyecto
+        /// </summary>
+        /// <param name="solicitudCambio">Objeto solicitud de cambio</param>
+        /// <returns>Lista de solicitudes</returns>
         public List<SolicitudCambio> ListarPorProyectoLineaBase(SolicitudCambio solicitudCambio)
         {
             return _solicitudCambioData.ListarPorProyectoLineaBase(solicitudCambio);
         }
 
+        /// <summary>
+        /// Obtiene una solicitud por el Id
+        /// </summary>
+        /// <param name="id">Id de la solicitud</param>
+        /// <returns>Objetp Solicitud de cambio</returns>
         public SolicitudCambio ObtenerPorId(int id)
         {
             return _solicitudCambioData.ObtenerPorId(id);
         }
 
+        /// <summary>
+        /// Obtiene el archivo de la solicitud de cambio
+        /// </summary>
+        /// <param name="id">Id de la solicitud</param>
+        /// <returns>Archivo de la solicitud</returns>
         public SolicitudCambio ObtenerArchivo(int id)
         {
             SolicitudCambio solicitudCambio = null;
@@ -49,6 +75,10 @@ namespace TMD.CF.LogicaNegocios.Implementacion
             return solicitudCambio;
         }
 
+        /// <summary>
+        /// Actualiza el archivo de una solicitud
+        /// </summary>
+        /// <param name="solicitudCambio">Objeto solicutd a actualziar</param>
         public void ActualizarArchivo(SolicitudCambio solicitudCambio)
         {
             using (var scope = new TransactionScope())
