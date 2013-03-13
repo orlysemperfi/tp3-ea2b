fbe258f74b63409323afe236ddbdcfb60994b132
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-13 04:22:42 +0000

    CF : * Se agregaron comentarios ala clase OrdenCambioLogica

--- branches/CF/TMD.CF.LogicaNegocios/Contrato/IOrdenCambioLogica.cs
+++ branches/CF/TMD.CF.LogicaNegocios/Contrato/IOrdenCambioLogica.cs
@@ -6,8 +6,17 @@ using TMD.Entidades;
 
 namespace TMD.CF.LogicaNegocios.Contrato
 {
+    /// <summary>
+    /// Contrato  de la logica d enegocios de la entidad orden de cambio.
+    /// </summary>
     public interface IOrdenCambioLogica
     {
+        /// <summary>
+        /// Lista las ordenes de un proyecto
+        /// </summary>
+        /// <param name="codigoProyecto">Codigo proyecto</param>
+        /// <param name="codigoLineaBase">Codigo linea Base</param>
+        /// <returns>Lista Orden de cambio</returns>
         List<OrdenCambio> ListarPorProyectoLBase(int codigoProyecto, int codigoLineaBase);
     }
 }
