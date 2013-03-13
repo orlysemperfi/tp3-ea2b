6adbe7343965df8dd8ddf3c9da83856620850418
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-13 04:22:42 +0000

    CF : * Se agregaron comentarios ala clase OrdenCambioLogica

--- TMD.CF.LogicaNegocios/Implementacion/OrdenCambioLogica.cs
+++ TMD.CF.LogicaNegocios/Implementacion/OrdenCambioLogica.cs
@@ -8,6 +8,9 @@ using TMD.Entidades;
 
 namespace TMD.CF.LogicaNegocios.Implementacion
 {
+    /// <summary>
+    /// Impelemntacion  de la logica d enegocios de la entidad orden de cambio.
+    /// </summary>
     public class OrdenCambioLogica : IOrdenCambioLogica
     {
         private readonly IOrdenCambioData _ordenCambioData;
@@ -17,6 +20,12 @@ namespace TMD.CF.LogicaNegocios.Implementacion
             _ordenCambioData = ordenCambioData;
         }
 
+        /// <summary>
+        /// Lista las ordenes de un proyecto
+        /// </summary>
+        /// <param name="codigoProyecto">Codigo proyecto</param>
+        /// <param name="codigoLineaBase">Codigo linea Base</param>
+        /// <returns>Lista Orden de cambio</returns>
         public List<OrdenCambio> ListarPorProyectoLBase(int codigoProyecto, int codigoLineaBase)
         {
             return _ordenCambioData.ListarPorProyectoLBase(codigoProyecto, codigoLineaBase);
