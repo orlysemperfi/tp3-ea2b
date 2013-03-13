8c19b832c7ee834e816902e6b6f2e741db1815a6
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-13 04:22:19 +0000

    CF : * Se agregaron comentairos  ala clase OrdenCambioData

--- TMD.CF.AccesoDatos/Contrato/IOrdenCambioData.cs
+++ TMD.CF.AccesoDatos/Contrato/IOrdenCambioData.cs
@@ -6,8 +6,17 @@ using TMD.Entidades;
 
 namespace TMD.CF.AccesoDatos.Contrato
 {
+    /// <summary>
+    /// Contrato  del Acceso a datos de la entidad orden de cambio.
+    /// </summary>
     public interface IOrdenCambioData
     {
+        /// <summary>
+        /// Lista las ordenes de un proyecto
+        /// </summary>
+        /// <param name="codigoProyecto">Codigo proyecto</param>
+        /// <param name="codigoLineaBase">Codigo linea Base</param>
+        /// <returns>Lista Orden de cambio</returns>
         List<OrdenCambio> ListarPorProyectoLBase(int codigoProyecto, int codigoLineaBase);
     }
 }
