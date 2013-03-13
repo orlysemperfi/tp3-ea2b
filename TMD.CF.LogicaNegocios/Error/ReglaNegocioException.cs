1fad1b46798cb33d474ae3675d1bdcee868227a3
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-13 04:41:19 +0000

    CF : + Se agrego la clase ReglaNegocioException

---
+++ TMD.CF.LogicaNegocios/Error/ReglaNegocioException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TMD.CF.LogicaNegocios.Error
+{
+    /// <summary>
+    /// Representa una excepcion de logica de negocio
+    /// </summary>
+    public class ReglaNegocioException : Exception
+    {
+        public ReglaNegocioException(String mensaje):base(mensaje)
+        {
+            
+        }
+    }
+}
