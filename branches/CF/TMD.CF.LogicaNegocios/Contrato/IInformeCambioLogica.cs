a563a8e2b524d48d47e6af5201f4ffcd0c64e711
jacardenasm@gmail.com <jacardenasm@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-13 06:05:13 +0000

    CF : + Agregado las clases para la entidad InformeCambio

---
+++ branches/CF/TMD.CF.LogicaNegocios/Contrato/IInformeCambioLogica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMD.Entidades;
+
+namespace TMD.CF.LogicaNegocios.Contrato
+{
+    public interface IInformeCambioLogica
+    {
+        void Agregar(InformeCambio informeCambio);
+        void Aprobar(InformeCambio informeCambio);
+        List<InformeCambio> ListarPorLineaBase(InformeCambio informeCambio);
+        InformeCambio ObtenerPorId(int id);
+    }
+}
