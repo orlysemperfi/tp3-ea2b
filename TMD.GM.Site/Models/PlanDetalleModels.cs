9d6a68242728a7efa2ff1c4722940fb27b1cdac5
eddysz2000@gmail.com <eddysz2000@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-14 06:09:19 +0000

    Modelo

---
+++ TMD.GM.Site/Models/PlanDetalleModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+using TMD.GM.Entidades;
+
+namespace TMD.GM.Site.Models
+{
+    public class PlanDetalleModel
+    {
+        public string codigoPlan { get; set; }
+        public int item { get; set; }
+        public List<SelectListItem> listaTA { get; set; }
+        public List<SelectListItem> listaTU { get; set; }
+        public List<SelectListItem> listaFR { get; set; }
+    }
+}
---
+++ TMD.GM.Site/Models/PlanModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+using TMD.GM.Entidades;
+
+namespace TMD.GM.Site.Models
+{
+    public class PlanModel
+    {
+        public List<PlanDetalleBE> listaDetalle { get; set; }
+        public string codigoPlan { get; set; }
+        public bool activo { get; set; }
+       
+    }
+}
