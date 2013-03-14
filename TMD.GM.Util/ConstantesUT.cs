f0f20b560dddeb3b4087f4532a97fa01537b13f3
msilvaflores@gmail.com <msilvaflores@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-14 05:25:51 +0000

    Solucion con constantes de aplicacion o librerias utiles

---
+++ TMD.GM.Util/ConstantesUT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TMD.GM.Util
+{
+    public static class ConstantesUT
+    {
+        [SuppressMessage("Microsoft.Performance", "CA1815:OverrideEqualsAndOperatorEqualsOnValueTypes")]
+        public struct ESTADO_SOLICITUD
+        {
+            public const int Aperturado = 1;
+        }
+    }
+}
