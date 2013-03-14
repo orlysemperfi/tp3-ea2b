4778c4009821d85e1e8efbd72e4ff1e5e2cbd90d
cadiazoliva@gmail.com <cadiazoliva@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-14 05:15:42 +0000

    Agregando Entidades de Gestion de Mantenimiento

---
+++ TMD.GM.Entidades/PlanBE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace TMD.GM.Entidades
+{
+    public class PlanBE
+    {
+        public PlanBE()
+        { }
+        public PlanBE(IDataReader oReader)
+        {}
+
+        #region Atributos
+        public string CODIGO { get; set; }
+        #endregion
+    }
+}
---
+++ TMD.GM.Entidades/SolicitudBE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace TMD.GM.Entidades
+{
+    public class SolicitudBE
+    {
+        public SolicitudBE()
+        { }
+        public SolicitudBE(IDataReader oReader)
+        {}
+
+        #region Atributos
+        public string NUMERO_SOLICITUD { get; set; }
+        public DateTime FECHA_SOLICITUD { get; set; }
+        #endregion
+    }
+}
