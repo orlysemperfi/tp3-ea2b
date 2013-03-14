e1821270713e765ebf2d0ef67ac6cd724a48095a
baigon932@gmail.com <baigon932@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-14 16:19:52 +0000

    MP: CUS006 Cambiar estado Propuesta "En Desarrollo"

---
+++ TMD.Site/Vistas/MP/PropuestaMejoraDesarrollo.aspx
@@ -0,0 +1,109 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Vistas/MP/TMD-MP.Master" AutoEventWireup="true" CodeBehind="PropuestaMejoraDesarrollo.aspx.cs" Inherits="TMD.CF.Site.Vistas.MP.PropuestaMejoraDesarrollo" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+
+<div class="contenedor-pagina">
+    <div class="contenedor-pagina-titulo">
+        DESARROLLAR PROPUESTA DE MEJORA
+    </div>
+    <br />
+    <table border="0" cellpadding="0" cellspacing="0">
+        <tr>
+            <td>
+                Código:
+            </td>
+            <td class="textbox-espaciado">
+                <asp:TextBox ID="tbxCodigo" runat="server" CssClass="estilo_textbox" Width="150px"></asp:TextBox>
+            </td>
+            <td class="textbox-espaciado">
+                Tipo:
+            </td>
+            <td class="textbox-espaciado">
+                <asp:DropDownList ID="ddlTipo" runat="server" CssClass="estilo_combobox">
+                </asp:DropDownList>
+            </td>
+            <td class="textbox-espaciado">
+                Fecha del:
+            </td>
+            <td class="textbox-espaciado">
+                <asp:TextBox ID="tbxFechaInicio" runat="server" Width="78px"></asp:TextBox>
+            </td>
+            <td class="textbox-espaciado">
+                al: 
+            </td>
+            <td class="textbox-espaciado">
+                <asp:TextBox ID="tbxFechaFin" runat="server" Width="67px"></asp:TextBox>
+            </td>
+            <td class="textbox-espaciado">
+                <asp:LinkButton ID="lbtnBuscar" runat="server" OnClick="lbtnBuscar_Click" CssClass="estilo_boton" Text="Buscar"></asp:LinkButton>
+            </td>
+        </tr>
+    </table>
+
+    <div id="divMensaje" runat="server">
+        <br />
+        <table border="0" cellpadding="0" cellspacing="0">
+            <tr>
+                <td>
+                    <asp:Label ID="lblMensajeError" runat="server"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <br />
+
+    <table border="0" cellpadding="0" cellspacing="0">
+        
+            <tr>
+                <td>
+                    <asp:GridView ID="gvwPropuestaMejoraListado" runat="server" AutoGenerateColumns="false" 
+                        DataSource='<%#ObtenerPropuestaMejoraListado() %>' 
+                        OnRowCommand="gvwPropuestaMejoraDesarrollo_RowCommand" 
+                        Width="657px">
+                        <Columns>
+                            
+                            <asp:TemplateField HeaderText="Codigo Propuesta">
+                                <ItemTemplate>
+                                    <asp:Label ID="lblCodigo" runat="server" Text='<%#Eval("CODIGO_PROPUESTA") %>'/>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Nombre Propuesta">
+                                <ItemTemplate>
+                                    <asp:Label ID="lblLimSuperior" runat="server" Text='<%#Eval("DESCRIPCION") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Estado">
+                                <ItemTemplate>
+                                    <asp:Label ID="lblLimInferior" runat="server" Text='<%#Eval("NOMBRE_ESTADO") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Fecha">
+                                <ItemTemplate>
+                                    <asp:Label ID="lblCalificacion" runat="server" Text='<%#Eval("FECHA_REGISTRO") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>                        
+                        </Columns>                        
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <table border="0" cellpadding="0" cellspacing="0">
+            <tr>
+                <td>
+                    <asp:LinkButton ID="lbtnCambiarEstadoEnDesarrollo" runat="server" OnClick="ibtnCambiarEstadoEnDesarrollo_Click" CssClass="estilo_boton" Text="En Desarrollo"></asp:LinkButton>
+                </td>
+                <td class="boton-espaciado">
+                    <asp:LinkButton ID="lbtnSalir" runat="server" OnClick="ibtnSalir_Click" CssClass="estilo_boton" Text="Salir"></asp:LinkButton>
+                </td>
+            </tr>
+        </table>
+
+</div>
+
+
+
+</asp:Content>
---
+++ TMD.Site/Vistas/MP/PropuestaMejoraDesarrollo.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using TMD.Entidades;
+using TMD.MP.Comun;
+using System.Web.UI.WebControls;
+using TMD.MP.LogicaNegocios.Contrato;
+using TMD.MP.LogicaNegocios.Implementacion;
+
+namespace TMD.CF.Site.Vistas.MP
+{
+    public partial class PropuestaMejoraDesarrollo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack){
+                CargarTipoPropuesta();
+                CargarPropuestaMejoraListado();                
+            }
+
+        }
+
+        protected void CargarTipoPropuesta()
+        {
+            ddlTipo.Items.Add(new ListItem("[Todos]", ""));
+            ddlTipo.Items.Add(new ListItem(Constantes.TIPO_PROPUESTA_PROBLEMA, Constantes.TIPO_PROPUESTA_PROBLEMA));
+            ddlTipo.Items.Add(new ListItem(Constantes.TIPO_PROPUESTA_MEJORA, Constantes.TIPO_PROPUESTA_MEJORA));
+            ddlTipo.SelectedIndex = 0;
+        }
+
+
+        protected void CargarPropuestaMejoraListado()
+        {
+            IPropuestaMejoraLogica oPropuestaMejoraLogica = PropuestaMejoraLogica.getInstance();
+            PropuestaMejoraEntidad oPropuestaMejoraFiltro = new PropuestaMejoraEntidad();
+
+            if (tbxCodigo.Text != null && tbxCodigo.Text != string.Empty)
+                oPropuestaMejoraFiltro.codigo_Propuesta = Convert.ToInt32(tbxCodigo.Text.ToString());
+            if (tbxFechaInicio.Text != null && tbxFechaInicio.Text != string.Empty)
+                oPropuestaMejoraFiltro.fecha_Registro_Inicio = Convert.ToDateTime(tbxFechaInicio.Text.ToString());
+            if (tbxFechaFin.Text != null && tbxFechaFin.Text != string.Empty)
+                oPropuestaMejoraFiltro.fecha_Registro_Fin = Convert.ToDateTime(tbxFechaFin.Text.ToString());
+
+            oPropuestaMejoraFiltro.tipo_Propuesta = ddlTipo.SelectedItem.Value;
+            List<PropuestaMejoraEntidad> oPropuestaMejoraColeccion = oPropuestaMejoraLogica.ObtenerPropuestaMejoraAsignadasListadoPorFiltros(oPropuestaMejoraFiltro);
+            Sesiones.PropuestaMejoraListadoRemover();
+            Sesiones.PropuestaMejoraListado = oPropuestaMejoraColeccion;
+            gvwPropuestaMejoraListado.DataBind();
+            //PageIndexChanging();
+            //lblMensajeError.Text = "";
+        }
+
+        protected List<PropuestaMejoraEntidad> ObtenerPropuestaMejoraListado()
+        {
+            List<PropuestaMejoraEntidad> propuestaMejoraListado = Sesiones.PropuestaMejoraListado;
+            if (propuestaMejoraListado == null)
+            {
+                return null;
+            }
+            else
+            {
+                if (propuestaMejoraListado.Count == 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return propuestaMejoraListado;
+                }
+            }
+        }
+        
+        protected void lbtnBuscar_Click(object sender, EventArgs e)
+        {
+            CargarPropuestaMejoraListado();
+        }
+
+
+        protected void gvwPropuestaMejoraDesarrollo_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+            IPropuestaMejoraLogica oPropuestaMejoraLogica = PropuestaMejoraLogica.getInstance();
+            if (e.CommandName == "EditarPropuesta")
+            {
+                PropuestaMejoraEntidad oPropuestaMejora = oPropuestaMejoraLogica.ObtenerPropuestaMejoraPorCodigo(Convert.ToInt32(e.CommandArgument));
+                Sesiones.PropuestaMejoraSeleccionada = oPropuestaMejora;
+                if (oPropuestaMejora.codigo_Estado == Convert.ToInt32(Constantes.ESTADO_PROPUESTA.REGISTRADA))
+                    Response.Redirect(Paginas.TMD_MP_PropuestaMejoraFormulario + "?Action=" + Constantes.ACTION_UPDATE, true);
+                else
+                    Response.Redirect(Paginas.TMD_MP_PropuestaMejoraFormulario + "?Action=" + Constantes.ACTION_VIEW, true);
+            }
+            else if (e.CommandName == "EliminarPropuesta")
+            {
+                PropuestaMejoraEntidad oPropuestaMejora = oPropuestaMejoraLogica.ObtenerPropuestaMejoraPorCodigo(Convert.ToInt32(e.CommandArgument));
+                String strMensaje = oPropuestaMejoraLogica.BorrarPropuestaMejora(oPropuestaMejora);
+                if (strMensaje != null)
+                {
+                    lblMensajeError.Text = strMensaje;
+                }
+                else
+                {
+                    CargarPropuestaMejoraListado();
+                }
+            }
+        }
+
+        protected void ibtnCambiarEstadoEnDesarrollo_Click(object sender, EventArgs e)
+        {
+            Sesiones.PropuestaMejoraSeleccionadaRemover();
+            Response.Redirect(Paginas.TMD_MP_PropuestaMejoraFormulario + "?Action=" + Constantes.ACTION_INSERT, true);
+        }
+
+        protected void ibtnSalir_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(Paginas.TMD_MP_Inicio, true);
+        }
+
+    }
+
+
+}
