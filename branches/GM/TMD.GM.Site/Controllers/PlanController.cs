a0ddda9421c70113d8f6bdc97e0d35cc46c45f9a
cadiazoliva@gmail.com <cadiazoliva@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-14 06:11:25 +0000

    Controlador PLAN basico

---
+++ branches/GM/TMD.GM.Site/Controllers/PlanController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMD.GM.LogicaNegocios;
+using TMD.GM.Site.Models;
+using System.Web.Helpers;
+using TMD.GM.Entidades;
+
+namespace TMD.GM.Site.Controllers
+{
+    public class PlanController : Controller
+    {
+        //
+        // GET: /Plan/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Plan()
+        {
+            ComunBL ComunBL = new ComunBL();
+            PlanModel model = new PlanModel();
+            PlanBE oPlanBE = ComunBL.ObtenerObtenerPlanNuevo();
+            model.codigoPlan = oPlanBE.CODIGO;
+
+            model.listaDetalle = new List<PlanDetalleBE>();
+            model.listaDetalle.Add(new PlanDetalleBE());
+            return View(model);
+        }
+        public ActionResult PlanDetalle()
+        {
+            ComunBL ComunBL = new ComunBL();
+            PlanDetalleModel model = new PlanDetalleModel();
+
+            List<SelectListItemBE> listaDataTA = new List<SelectListItemBE>();
+            List<SelectListItemBE> listaDataTU = new List<SelectListItemBE>();
+
+            listaDataTA = ComunBL.ListarTipoActividad();
+            listaDataTU = ComunBL.ListarTiempoUniMed();
+
+            model.listaTA = new List<SelectListItem>();
+            model.listaTU = new List<SelectListItem>();
+
+            foreach (var item in listaDataTA)
+            {
+                model.listaTA.Add(new SelectListItem() { Selected = false, Value = item.CODIGO.ToString(), Text = item.DESCRIPCION });
+            }
+            foreach (var item in listaDataTU)
+            {
+                model.listaTU.Add(new SelectListItem() { Selected = false, Value = item.CODIGO.ToString(), Text = item.DESCRIPCION });
+            }
+
+            return View(model);
+        }
+
+        public ActionResult PlanDetallePV()
+        {
+            ComunBL ComunBL = new ComunBL();
+            PlanDetalleModel model = new PlanDetalleModel();
+
+            List<SelectListItemBE> listaDataTA = new List<SelectListItemBE>();
+            List<SelectListItemBE> listaDataTU = new List<SelectListItemBE>();
+
+            listaDataTA = ComunBL.ListarTipoActividad();
+            listaDataTU = ComunBL.ListarTiempoUniMed();
+
+            model.listaTA = new List<SelectListItem>();
+            model.listaTU = new List<SelectListItem>();
+
+            foreach (var item in listaDataTA)
+            {
+                model.listaTA.Add(new SelectListItem() { Selected = false, Value = item.CODIGO.ToString(), Text = item.DESCRIPCION });
+            }
+            foreach (var item in listaDataTU)
+            {
+                model.listaTU.Add(new SelectListItem() { Selected = false, Value = item.CODIGO.ToString(), Text = item.DESCRIPCION });
+            }
+
+            return PartialView(model);
+        }
+    }
+}
