d73497cc819f945eaf31210f1b8412d09e5ead48
alyssa.quinto@gmail.com <alyssa.quinto@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-14 03:53:17 +0000

    MP: cambios

--- TMD.MP.AccesoDatos/Implementacion/SolucionMejoraDataSql.cs
+++ TMD.MP.AccesoDatos/Implementacion/SolucionMejoraDataSql.cs
@@ -21,14 +21,53 @@ namespace TMD.MP.AccesoDatos.Implementacion
             String strConn = ConfigurationManager.ConnectionStrings[Constantes.TMD_MP_DATABASE].ConnectionString;
             SqlConnection sqlConn = new SqlConnection(strConn);
             StringBuilder strSQL = new StringBuilder();
-            
+            strSQL.Append("SELECT S.CODIGO, S.DESCRIPCION, P.DESCRIPCION, E.NOMBRE ");
+            strSQL.Append("FROM MP.SOLUCION_MEJORA S ");
+            strSQL.Append("INNER JOIN MP.PROPUESTAMEJORA P ON S.CODIGO_PROPUESTA = P.CODIGO_PROPUESTA ");
+            strSQL.Append("INNER JOIN MP.ESTADO E ON S.CODIGO_ESTADO = E.CODIGO ");
+            strSQL.Append("WHERE E.NOMBRE <> '" + Constantes.ESTADO_SOLUCION_ELIMINADA + "' ");
+            if (oSolucionMejoraFiltro != null)
+            {
+                if (oSolucionMejoraFiltro.codigo_Propuesta != null && oSolucionMejoraFiltro.codigo_Propuesta != 0)
+                    strSQL.Append("AND S.CODIGO = @CODIGO_SOLUCION ");
+                if (oSolucionMejoraFiltro.propuesta != String.Empty)
+                    strSQL.Append("AND P.DESCRIPCION = @PROPUESTA ");
+                if (oSolucionMejoraFiltro.fecha_Registro_Inicio != null)
+                    strSQL.Append("AND DATEDIFF(DAY, S.FECHA_APROBACION, @FECHA_REGISTRO_INICIO) <= 0 ");
+                if (oSolucionMejoraFiltro.fecha_Registro_Inicio != null)
+                    strSQL.Append("AND DATEDIFF(DAY, S.FECHA_APROBACION, @FECHA_REGISTRO_FIN) >= 0  ");
+            }
+
             SqlCommand sqlCmd = new SqlCommand(strSQL.ToString(), sqlConn);
             SqlDataReader dr = null;
             sqlCmd.CommandType = CommandType.Text;
 
+            if (oSolucionMejoraFiltro != null)
+            {
+                if (oSolucionMejoraFiltro.codigo_Propuesta != null && oSolucionMejoraFiltro.codigo_Propuesta != 0)
+                    sqlCmd.Parameters.Add("@CODIGO_SOLUCION", SqlDbType.Int).Value = oSolucionMejoraFiltro.codigo_Propuesta;
+                if (oSolucionMejoraFiltro.propuesta != String.Empty)
+                    sqlCmd.Parameters.Add("@PROPUESTA", SqlDbType.VarChar).Value = oSolucionMejoraFiltro.propuesta;
+                if (oSolucionMejoraFiltro.fecha_Registro_Inicio != null)
+                    sqlCmd.Parameters.Add("@FECHA_REGISTRO_INICIO", SqlDbType.DateTime).Value = oSolucionMejoraFiltro.fecha_Registro_Inicio;
+                if (oSolucionMejoraFiltro.fecha_Registro_Inicio != null)
+                    sqlCmd.Parameters.Add("@FECHA_REGISTRO_FIN", SqlDbType.DateTime).Value = oSolucionMejoraFiltro.fecha_Registro_Fin;
+            }
+
             try
             {
-
+                sqlConn.Open();
+                dr = sqlCmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    oSolucionMejora = new SolucionMejoraEntidad();
+                    oSolucionMejora.codigo_Solucion = Utilitario.getDefaultOrIntDBValue(dr["CODIGO"]);
+                    oSolucionMejora.solucion = Utilitario.getDefaultOrStringDBValue(dr["DESCRIPCION"]);
+                    oSolucionMejora.propuesta = Utilitario.getDefaultOrStringDBValue(dr["DESCRIPCION"]);
+                    oSolucionMejora.nombre_Estado = Utilitario.getDefaultOrStringDBValue(dr["NOMBRE"]);
+                    oSolucionMejoraColeccion.Add(oSolucionMejora);
+                }
+                dr.Close();
                 return oSolucionMejoraColeccion;
             }
             catch (System.Exception ex)
