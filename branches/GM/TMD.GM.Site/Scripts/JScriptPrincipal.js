13823c18bf2ef02d97336ebfb9d54fcf6b525c49
cadiazoliva@gmail.com <cadiazoliva@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-14 18:47:37 +0000

    Estructura de la capa de Presentacion

--- branches/GM/TMD.GM.Site/Scripts/JScriptPrincipal.js
+++ branches/GM/TMD.GM.Site/Scripts/JScriptPrincipal.js
@@ -12,7 +12,9 @@
         $(this).hide();
     });
     $('#pnlPlanDetalle').dialog({ autoOpen: false, width: 548, height: 380 }).parent('.ui-dialog').find('.ui-dialog-titlebar-close').hide();
-
+    $('#divNuevoPlan').dialog({ autoOpen: false, width: 548, height: 380 }).parent('.ui-dialog').find('.ui-dialog-titlebar-close').hide();
+    $('#divNuevaSolicitud').dialog({ autoOpen: false, width: 680, height: 400 }).parent('.ui-dialog').find('.ui-dialog-titlebar-close').hide();
+    
 
 
 });
@@ -39,7 +41,77 @@ function Plan_BtnAgregar_click(ParamUrl1) {
         }
     });
 }
+function BuscarPlanesMant(ParamUrl1) {
 
+    $.ajax({
+        type: "post",
+        url: ParamUrl1,
+        data: "",
+        cache: false,
+        success: function (data, textStatus, jqXHR) {
+            $("#divGrid").html(data);
+        },
+        error: function (req, status, error) {
+            alert("fail: " + req + " " + status + " " + error);
+        },
+        complete: function () {
+        }
+    });
+}
+function BuscarSolicitudes(ParamUrl1) {
+
+    $.ajax({
+        type: "post",
+        url: ParamUrl1,
+        data: "",
+        cache: false,
+        success: function (data, textStatus, jqXHR) {
+            $("#divGridSolicitudesConsulta").html(data);
+        },
+        error: function (req, status, error) {
+            alert("fail: " + req + " " + status + " " + error);
+        },
+        complete: function () {
+        }
+    });
+}
+function PlanMantNuevo(ParamUrl1) {
+
+    $.ajax({
+        type: "post",
+        url: ParamUrl1,
+        data: "",
+        cache: false,
+        success: function (data, textStatus, jqXHR) {
+            $("#divNuevoPlan").html(data);
+            MostrarModalPlanNuevo();
+        },
+        error: function (req, status, error) {
+            alert("fail: " + req + " " + status + " " + error);
+        },
+        complete: function () {
+        }
+    });
+}
+
+function SolicitudNueva(ParamUrl1) {
+
+    $.ajax({
+        type: "post",
+        url: ParamUrl1,
+        data: "",
+        cache: false,
+        success: function (data, textStatus, jqXHR) {
+            $("#divNuevaSolicitud").html(data);
+            MostrarModalSolicitudNueva();
+        },
+        error: function (req, status, error) {
+            alert("fail: " + req + " " + status + " " + error);
+        },
+        complete: function () {
+        }
+    });
+}
 function Plan_BtnAceptarDetalle_click(ParamUrl1) {
 
     $.ajax({
@@ -67,7 +139,22 @@ function MostrarModalPlanDetalle() {
     $('#pnlPlanDetalle').dialog('option', 'modal', true).dialog('open');
     return true;
 }
-
 function CerrarModalPlanDetalle(ParamUrl1, ParamUrl2) {
     $('#pnlPlanDetalle').dialog('option', 'modal', true).dialog('close');
+};
+
+function MostrarModalPlanNuevo() {
+    $('#divNuevoPlan').dialog('option', 'modal', true).dialog('open');
+    return true;
+}
+function CerrarModalPlanNuevo( ) {
+    $('#divNuevoPlan').dialog('option', 'modal', true).dialog('close');
+};
+
+function MostrarModalSolicitudNueva() {
+    $('#divNuevaSolicitud').dialog('option', 'modal', true).dialog('open');
+    return true;
+}
+function CerrarModalSolicitudNueva() {
+    $('#divNuevaSolicitud').dialog('option', 'modal', true).dialog('close');
 };
