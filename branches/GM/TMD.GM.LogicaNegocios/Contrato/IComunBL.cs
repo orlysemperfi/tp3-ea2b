22516e7647d0d833bbe65134ca794ffb61c73be0
cadiazoliva@gmail.com <cadiazoliva@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-14 18:36:30 +0000

    Estructura de capa de negocio y refactor de ComunBL

---
+++ branches/GM/TMD.GM.LogicaNegocios/Contrato/IComunBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMD.GM.Entidades;
+
+namespace TMD.GM.LogicaNegocios.Contrato
+{
+    public interface IComunBL
+    {
+        List<SelectListItemBE> ListarTipoMante();
+        List<SelectListItemBE> ListarEstadoSolicitud();
+        List<SelectListItemBE> ListarTipoActividad();
+        List<SelectListItemBE> ListarTiempoUniMed();
+        List<SelectListItemBE> ListarPrioridad();
+        List<SelectListItemBE> ListarFrecuencia();
+    }
+}
