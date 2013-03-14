1dce838bcbadf3068a767aedfb07f29fafaef91c
jrufasto5@gmail.com <jrufasto5@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-14 18:38:50 +0000

    Implementar PlanBL y SolciitudBL

---
+++ TMD.GM.LogicaNegocios/Contrato/IPlanBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMD.GM.Entidades;
+
+namespace TMD.GM.LogicaNegocios.Contrato
+{
+    public interface IPlanBL
+    {
+        PlanBE ObtenerObtenerPlanNuevo();
+        List<SelectListItemBE> ListarPlanMante();
+        List<PlanBE> ListarPlanManteTodos();
+    }
+}
---
+++ TMD.GM.LogicaNegocios/Implementacion/PlanBL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMD.GM.AccesoDatos.Implementacion;
+using TMD.GM.Entidades;
+using TMD.GM.LogicaNegocios.Contrato;
+using TMD.GM.AccesoDatos.Contrato;
+namespace TMD.GM.LogicaNegocios.Implementacion
+{
+    public class PlanBL:IPlanBL
+    {
+        #region Constructor
+        private readonly IPlanDA instanciaDA;
+        public PlanBL()
+        {
+            instanciaDA = new PlanDA();
+        }
+        #endregion
+
+        public PlanBE ObtenerObtenerPlanNuevo()
+        {
+            return instanciaDA.ObtenerPlanNuevo();
+        }
+        public List<SelectListItemBE> ListarPlanMante()
+        {
+            return instanciaDA.ListarPlanMante();
+        }
+        public List<PlanBE> ListarPlanManteTodos()
+        {
+            return instanciaDA.ListarPlanManteTodos();
+        }
+    }
+}
