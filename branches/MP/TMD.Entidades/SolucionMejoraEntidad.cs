025908a0719e26b6e8f28c64db3fca3c7d38706d
alyssa.quinto@gmail.com <alyssa.quinto@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-14 03:53:17 +0000

    MP: cambios

--- branches/MP/TMD.Entidades/SolucionMejoraEntidad.cs
+++ branches/MP/TMD.Entidades/SolucionMejoraEntidad.cs
@@ -8,11 +8,15 @@ namespace TMD.Entidades
     public class SolucionMejoraEntidad
     {
         public int? codigo_Solucion { get; set; }
+        public String solucion { get; set; }
         public int? codigo_Propuesta { get; set; }
         public DateTime fecha_Envio { get; set; }
+        public int? codigo_Estado { get; set; }
         public DateTime? fecha_Registro_Inicio { get; set; }
         public DateTime? fecha_Registro_Fin { get; set; }
-        //public String? propuesta { get; set; }
+        public String propuesta { get; set; }
+        public String nombre_Estado { get; set; }
+
 
     }
 }
