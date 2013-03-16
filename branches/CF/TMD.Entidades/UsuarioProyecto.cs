bacdbd4b12a33019316d9dc97505f8ff203dda08
llacruze30@gmail.com <llacruze30@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-16 21:07:12 +0000

    CF : * Agregado comentarios de las entidades del módulo de CF

--- branches/CF/TMD.Entidades/UsuarioProyecto.cs
+++ branches/CF/TMD.Entidades/UsuarioProyecto.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace TMD.Entidades
 {
+    /// <summary>
+    /// Clase que representa la entidad usuario asignado a un proyecto
+    /// </summary>
     public class UsuarioProyecto : Base
     {
         public int Id { get; set; }
