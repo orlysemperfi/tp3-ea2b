5696c9339758ad699bb386c2215cf4b0360951bb
malopezap@gmail.com <malopezap@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-16 17:54:05 +0000

    [Services Desk] Adjuntar Documento

--- branches/SD/TMD.SD.LogicaNegocio-Atencion/Contrato/ITicketLogica.cs
+++ branches/SD/TMD.SD.LogicaNegocio-Atencion/Contrato/ITicketLogica.cs
@@ -28,6 +28,8 @@ namespace TMD.SD.LogicaNegocio_Atencion.Contrato
         void registrarSolucion(int numeroTicket, String solucion, int codigoEquipo, int codigoEspecialista);
         void registrarSeguimiento(SeguimientoTicket seguimientoTicket);
         //List<SeguimientoTicket> listaSeguimientos(int numeroTicket);
+
+        void registrarDocumentoTicket(DocumentoTicket documentoTicket);
         
     }
 }
