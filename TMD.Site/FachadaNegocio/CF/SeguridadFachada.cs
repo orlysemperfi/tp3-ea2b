dd83019bd0f17051febc813c812386867da24aee
berczeck69@gmail.com <berczeck69@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-16 20:17:25 +0000

    CF : Se agrego la fachada de SeguridadFachada

---
+++ TMD.Site/FachadaNegocio/CF/SeguridadFachada.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TMD.CF.LogicaNegocios.Contrato;
+using System.Configuration;
+using TMD.CF.LogicaNegocios.Implementacion;
+using TMD.CF.AccesoDatos.Implementacion;
+using TMD.Entidades;
+
+namespace TMD.CF.Site.FachadaNegocio.CF
+{
+    class SeguridadFachada
+    {
+        private readonly IUsuarioLogica UsuarioLogica;
+
+        public SeguridadFachada()
+        {
+            string baseDatos = ConfigurationManager.AppSettings["BaseDatos"];
+            UsuarioLogica = new UsuarioLogica(new UsuarioData(baseDatos));
+        }
+
+        public Usuario ObtenerUsuario(string login)
+        {
+            return UsuarioLogica.ObtenerUsuario(login);
+        }
+
+    }
+}
