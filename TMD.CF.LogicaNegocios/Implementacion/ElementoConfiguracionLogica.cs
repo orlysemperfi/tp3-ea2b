6771d49e57da69b76af2a31a9c8d77b11b21ed9b
screaber@gmail.com <screaber@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-17 16:47:01 +0000

    Referencia de log4net

--- TMD.CF.LogicaNegocios/Implementacion/ElementoConfiguracionLogica.cs
+++ TMD.CF.LogicaNegocios/Implementacion/ElementoConfiguracionLogica.cs
@@ -6,22 +6,26 @@ using TMD.CF.LogicaNegocios.Contrato;
 using TMD.Entidades;
 using TMD.CF.AccesoDatos.Contrato;
 using TMD.Core.Caching;
+using log4net;
 
 namespace TMD.CF.LogicaNegocios.Implementacion
 {
     /// <summary>
     /// Implementa la Logica de Negocio de la entidad Elemento de Configuracion.
     /// </summary>
+    
     public class ElementoConfiguracionLogica : IElementoConfiguracionLogica
     {
         private readonly IElementoConfiguracionData _elementoConfData;
         private readonly ICacheAdapter _cacheAdapter;
+        private static readonly ILog log = LogManager.GetLogger( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         //PATRON: INYECION DE DEPENDENCIA
         public ElementoConfiguracionLogica(IElementoConfiguracionData elementoConfData, ICacheAdapter cacheAdapter)
         {
             _elementoConfData = elementoConfData;
             _cacheAdapter = cacheAdapter;
+            log.Debug("Accesorios");
         }
 
         /// <summary>
@@ -33,7 +37,7 @@ namespace TMD.CF.LogicaNegocios.Implementacion
         {            
             List<ElementoConfiguracion> lista =
                 _cacheAdapter.Resolve(String.Format("_LisEcFase{0}",fase.Id), fase, _elementoConfData.ListarPorFase);
-
+            log.Debug("Nuevo");
             return lista;
         }
     }
