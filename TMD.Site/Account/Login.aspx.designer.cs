becc6067b398044d8b6c446cab0a23f83de66a42
kevinvcm@gmail.com <kevinvcm@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-17 22:04:49 +0000

    Uniformizar master template.

--- TMD.Site/Account/Login.aspx.designer.cs
+++ TMD.Site/Account/Login.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace TMD.CF.Site.Account {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Login LoginUser;
+        
+        /// <summary>
+        /// Control imgPeople.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgPeople;
     }
 }
