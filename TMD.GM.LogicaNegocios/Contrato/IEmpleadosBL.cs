d2d8395967d020fbe93dace8108211a7701e03f6
cadiazoliva@gmail.com <cadiazoliva@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-26 01:21:23 +0000

    MIGRACION AL TRUNK GRUPO 5 GESTION DE MANTENIMIENTO

---
+++ TMD.GM.LogicaNegocios/Contrato/IEmpleadosBL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMD.GM.Entidades;
+
+namespace TMD.GM.LogicaNegocios.Contrato
+{
+    public interface IEmpleadosBL
+    {
+        List<EmpleadosBE> ObtenerEmpleados(string nombres);
+        List<EspecialidadBE> RegistrarActividad(int empleado, int actividad, ref string mensaje, ref bool registrado);
+        void Eliminar_Actividad_Empleado(int empleado, int actividad);
+        void Especialidad_Actividad_Empleado(int empleado, int actividad, bool especialidad);
+    }
+}
