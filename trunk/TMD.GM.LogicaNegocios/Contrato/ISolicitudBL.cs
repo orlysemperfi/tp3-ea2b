07f76835c9a1eba52a5dc95f6e6d8ab8017589ae
cadiazoliva@gmail.com <cadiazoliva@gmail.com@1fea33c9-7adc-3942-9940-529586b6e928>
2013-03-26 01:21:23 +0000

    MIGRACION AL TRUNK GRUPO 5 GESTION DE MANTENIMIENTO

---
+++ trunk/TMD.GM.LogicaNegocios/Contrato/ISolicitudBL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMD.GM.Entidades;
+
+namespace TMD.GM.LogicaNegocios.Contrato
+{
+    public interface ISolicitudBL
+    {
+        SolicitudBE ObtenerSolicitudNueva();
+        List<SolicitudBE> ObtenerSolicitudes(SolicitudFiltroBE filtro);
+
+        void RegistrarSolicitud(SolicitudBE itemBE);
+        void ActualizarSolicitud(SolicitudBE itemBE);
+        void EliminarSolicitud(SolicitudBE itemBE);
+        SolicitudBE VisualizarSolicitud(SolicitudBE itemBE);
+
+        List<SolicitudDetalleBE> GenerarCronograma(SolicitudBE solicitudBE);
+        CronogramaBE ActividadesCronograma(DateTime fecha);
+    }
+}
